71bacca76928452ff6decfafefb30006554a5219
ma12iona <ma12iona@hotmail.com>
2018-04-13 23:12:00 +0700

    database

--- app/src/main/java/com/example/lenovo/selphies/Login.java
+++ app/src/main/java/com/example/lenovo/selphies/Login.java
@@ -9,21 +9,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private Button login;
     private Button register;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    private DatabaseReference user_ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupUI();
 
-        username = (EditText) findViewById(R.id.usernameText);
-        password = (EditText) findViewById(R.id.passwordText);
-        login = (Button) findViewById(R.id.loginButton);
-        register = (Button) findViewById(R.id.registerButton);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("users");
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +48,26 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    private void setupUI(){
+        username = (EditText) findViewById(R.id.usernameText);
+        password = (EditText) findViewById(R.id.passwordText);
+        login = (Button) findViewById(R.id.loginButton);
+        register = (Button) findViewById(R.id.registerButton);
+    }
+
     private void validate(String username, String password){
+        user_ref = ref.child(username);
+        user_ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        })
         if((username.equals("Admin")) && (password.equals("1234"))){
             Intent login = new Intent(Login.this, MainActivity.class);
             startActivity(login);
